8d65869349e8b54e952d8fbc045d2b3459cd7ae5
angersprite <ktlankton@gmail.com>
2023-11-01 19:05:26 -0500

    import fixes

--- src/app/api/auth/userNameExists/[username]/route.ts
+++ src/app/api/auth/userNameExists/[username]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { userNameExists } from '../../../../services/AuthService'
+import { userNameExists } from '@/app/services/AuthService'
 
 export async function GET(req: NextRequest, { params }: { params: { username: string } }) {
     const exists = await userNameExists(params.username)
